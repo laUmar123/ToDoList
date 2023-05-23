d1706350bef83a2f9776db497a6415d8389e9893
Umar <work.umar7@hotmail.com>
2023-05-23 01:44:04 +0100

    added the last two functions to the projectConfiguration modules that allow the project to rename their project and confirm the rename

--- src/projectConfiguration.js
+++ src/projectConfiguration.js
@@ -31,3 +31,36 @@ function deleteProject(e) {
         allTasksBtnFunctionality();
     };
 };
+
+/**
+ * If the user does press the rename button, this function will execute by showing the rename form
+ * @param {object} e self generated event object when used on an event listener  
+ */
+function toggleProjectRename(e) {
+    const getProjectParentElement = e.target.closest('.project');
+    if (e.target.classList.contains('rename-project-btn')) {
+        getProjectParentElement.querySelector('.project-name').classList.toggle('hide');
+        getProjectParentElement.querySelector('.rename-project').classList.toggle('hide');
+        e.target.closest('.project-configuration').classList.toggle('hide');
+    };
+};
+
+/**
+ * If the user presses the confirm button, the screen updates the display with the new project name, while also changing the project name for that instance
+ * @param {object} e self generated event object when used on an event listener 
+ */
+function confirmRename(e) {
+    const getProjectParentElement = e.target.closest('.project');
+    if (e.target.classList.contains('confirm-rename-btn')) {
+        for (let project of allProjects) {
+            if (project.projectName === getProjectParentElement.querySelector('.project-name').innerText && project.projectNum === Number(getProjectParentElement.dataset.project)) project.projectName = `${e.target.previousElementSibling.value}`;
+        }
+        getProjectParentElement.querySelector('.project-name').innerHTML = `${e.target.previousElementSibling.value}`;
+        getProjectParentElement.querySelector('.project-name').classList.toggle('hide');
+        e.target.parentElement.classList.toggle('hide');
+        getProjectParentElement.querySelector('.project-configuration').classList.toggle('hide');
+        updateAllProjectsStringified();
+    };
+};
+
+export { toggleProjectConfiguration, deleteProject, toggleProjectRename, confirmRename }
